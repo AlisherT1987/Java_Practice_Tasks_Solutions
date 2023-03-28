e2abdac2b17caddf308194bf17eebce0b1b34ece
Alisherb28 <alish.tussupbaev@gmail.com>
2023-03-28 13:37:35 -1000

    123

--- src/day02_escapeSequences/AboutMe.java
+++ src/day02_escapeSequences/AboutMe.java
@@ -5,7 +5,7 @@ public class AboutMe {
     public static void main(String[] args) {
 
         System.out.println("\tMy favorite music is \"Rock\" musics. \n\t\t\t\tMy favorite book is \"Data Structure in Java\"");
-
+        System.out.println("----------------------");
     }
 
 }
